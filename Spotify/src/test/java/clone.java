1988ce7e5406e96b18b48afb3bbb73caa10915a9
sarathjayaseelan4 <sarathjayaseelan4@gmail.com>
2025-11-11 11:14:50 +0530

    modified the test code

--- Spotify/src/test/java/clone.java
+++ Spotify/src/test/java/clone.java
@@ -21,46 +21,57 @@ public class clone {
     WebDriverWait wait;
 
     @BeforeClass
-    public void setup(){
-
-        ChromeOptions opt = new ChromeOptions();
-        opt.addArguments("--start-maximized");
-        driver = new ChromeDriver(opt);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    public void setup() {
+        try {
+            ChromeOptions opt = new ChromeOptions();
+            opt.addArguments("--start-maximized");
+            driver = new ChromeDriver(opt);
+            wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        } catch (Exception e) {
+            System.err.println("Failed during setup: " + e.getMessage());
+            Assert.fail("Browser setup failed", e);
+        }
     }
 
     @Test
-    public void count() throws InterruptedException {
-        driver.get("https://open.spotify.com/");
+    public void count() {
+        try {
+            driver.get("https://open.spotify.com/");
 
-        WebElement search  = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@data-testid='search-input']")));
-        search.click();
+            WebElement search = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@data-testid='search-input']")));
+            search.click();
 
-        search.sendKeys("lofi");
+            search.sendKeys("lofi");
 
-        search.sendKeys(Keys.ENTER);
+            search.sendKeys(Keys.ENTER);
 
-        WebElement playlist = driver.findElement(By.linkText("Lofi Songs (Sukoon Vibes) \uD83D\uDC9C"));
-        playlist.click();
+            WebElement playlist = driver.findElement(By.linkText("Lofi Songs (Sukoon Vibes) \uD83D\uDC9C"));
+            playlist.click();
 
-         String list = driver.findElement(By.cssSelector("span.gRs15KFAiBRH4FFbVvVr:nth-child(1)")).getText();
+            String list = driver.findElement(By.cssSelector("span.gRs15KFAiBRH4FFbVvVr:nth-child(1)")).getText();
 
-        String check = "Lofi Songs (Sukoon Vibes) 💜";
+            String check = "Lofi Songs (Sukoon Vibes) 💜";
 
-        WebElement words = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='main-view']/div/div[2]/div[1]/div/main/section/div[1]/div[2]/div[3]/span[2]/h1")));
-        words.getText();
+            WebElement words = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='main-view']/div/div[2]/div[1]/div/main/section/div[1]/div[2]/div[3]/span[2]/h1")));
+            String actualHeaderText = words.getText();
 
-        Assert.assertEquals(words,check,"NOt matched");
+            Assert.assertEquals(actualHeaderText, check, "NOt matched");
 
-        System.out.println(list);
-
-
-    @AfterClass
-    public void clossss(){
-        driver.close();
+            System.out.println(list);
+        } catch (Exception e) {
+            System.err.println("Test failed during execution: " + e.getMessage());
+            Assert.fail("Test failed with exception", e);
+        }
     }
 
-
-
-}
+    @AfterClass
+    public void clossss() {
+        try {
+            if (driver != null) {
+                driver.quit();
+            }
+        } catch (Exception e) {
+            System.err.println("Failed during browser teardown: " + e.getMessage());
+        }
+    }
